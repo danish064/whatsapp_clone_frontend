05fd66003d1a3180915d46974a306f46509c68a3
danish2016 <danish.danish.danish2016@gmail.com>
2024-03-01 17:07:21 +0500

    Init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="flex h-screen w-screen items-center justify-center bg-gray-50">
+    <div class="pb-20 text-6xl font-bold">HELLO</div>
+  </div>
+</template>
+
+<style scoped></style>
